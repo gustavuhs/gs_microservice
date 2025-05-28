a88c3e652677b9ece8a531391f313cacdc9509f7
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-28 19:18:24 -0300

    Criação de components

---
+++ src/app/components/inspiracao/inspiracao.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inspiracao',
+  standalone: true,
+  imports: [],
+  templateUrl: './inspiracao.component.html',
+  styleUrl: './inspiracao.component.css'
+})
+export class InspiracaoComponent {
+
+}
